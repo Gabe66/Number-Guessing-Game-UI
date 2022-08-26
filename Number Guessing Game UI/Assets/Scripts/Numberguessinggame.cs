2bd5f5ba6bd268d884478e8af41ee88b0f63cc8f
Gabe66 <111007541+Gabe66@users.noreply.github.com>
2022-08-26 11:30:57 -0700

    Scrips

--- Number Guessing Game UI/Assets/Scripts/Numberguessinggame.cs
+++ Number Guessing Game UI/Assets/Scripts/Numberguessinggame.cs
@@ -2,17 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NUmberguessinggame : MonoBehaviour
+public class NumberGuessingGame : MonoBehaviour
 {
+     private int guess = 5;
+    private int minValue = 1;
+    private int maxValue = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+       StartGame();
+    } 
 
     // Update is called once per frame
     void Update()
     {
-        
+    
+        if(Input.GetKeyDown(KeyCode.UpArrow)) 
+        {
+            minValue = guess;
+            guess = (maxValue + minValue) / 2;
+            Debug.Log("Is it higher or lower than: " + guess);
+        }
+    
+     if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            maxValue = guess;
+            guess = (maxValue + minValue) / 2;
+            Debug.Log("Is it higher or lower than: " + guess);
+        }
+      
+     if(Input.GetKeyDown(KeyCode.Return))
+        {      
+            Debug.Log("I guessed your number i'm a genius!");
+            Debug.Log("");
+            StartGame();
+        }
+    
+    }
+    
+    void StartGame()
+    {
+        guess =5;
+        minValue = 1;
+        maxValue = 10;
+
+        Debug.Log("Welcome to our Number Guessing Game.");
+       Debug.Log("The number range is from "+ minValue +"and" + maxValue);
+       Debug.Log("Guess a number between the given range.");
+       Debug.Log("Tell me if your number is higher or lower that: " + guess + "?");
+       Debug.Log("Push up arrow = Higher, Push down arrow = Lower, Push Enter = Correct");
+       maxValue = maxValue +1;
     }
 }
