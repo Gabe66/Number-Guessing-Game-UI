2e8ddbc53e9230972a25b3a26be68300a4632015
Gabe66 <111007541+Gabe66@users.noreply.github.com>
2022-09-02 11:12:42 -0700

    coding 10/2

--- Number Guessing Game UI/Assets/Scripts/Numberguessinggame.cs
+++ Number Guessing Game UI/Assets/Scripts/Numberguessinggame.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMpro;
+using TMPro;
 public class NumberGuessingGame : MonoBehaviour
 {
     
     [SerializeField]private int minValue = 1;
     [SerializeField]private int maxValue = 10;
-    [SerializedField]private TextMeshProUGUI GuessText;
+    [SerializeField]private TextMeshProUGUI guessText;
 
     private int guess;
     // Start is called before the first frame update
@@ -26,12 +26,12 @@ public class NumberGuessingGame : MonoBehaviour
     public void OnPressLower()
     {
         maxValue = guess - 1;
-        NextGuess;
+        NextGuess();
     }
-    Public void NextGuess()
+        public void NextGuess()
     {
         guess = Random.Range(minValue, maxValue + 1);
-        guessText.text = huess.ToString();
+        guessText.text = guess.ToString();
     }
     void StartGame()
     {
@@ -40,44 +40,5 @@ public class NumberGuessingGame : MonoBehaviour
 
 
 
-    // Update is called once per frame
-    void Update()
-    {
     
-        if(Input.GetKeyDown(KeyCode.UpArrow)) 
-        {
-            minValue = guess;
-            guess = (maxValue + minValue) / 2;
-            Debug.Log("Is it higher or lower than: " + guess);
-        }
-    
-     if(Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            maxValue = guess;
-            guess = (maxValue + minValue) / 2;
-            Debug.Log("Is it higher or lower than: " + guess);
-        }
-      
-     if(Input.GetKeyDown(KeyCode.Return))
-        {      
-            Debug.Log("I guessed your number i'm a genius!");
-            Debug.Log("");
-            StartGame();
-        }
-    
-    }
-    
-    void StartGame()
-    {
-        guess =5;
-        minValue = 1;
-        maxValue = 10;
-
-        Debug.Log("Welcome to our Number Guessing Game.");
-       Debug.Log("The number range is from "+ minValue +"and" + maxValue);
-       Debug.Log("Guess a number between the given range.");
-       Debug.Log("Tell me if your number is higher or lower that: " + guess + "?");
-       Debug.Log("Push up arrow = Higher, Push down arrow = Lower, Push Enter = Correct");
-       maxValue = maxValue +1;
-    }
 }
